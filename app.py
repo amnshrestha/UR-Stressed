e6afa6f6ce35ad42d831600bdb22f520a2d52c6f
Aman Shrestha <ashrest2@u.rochester.edu>
2021-04-03 22:23:00 -0400

    installed flask_restful

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import cv2
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)
-
 def generate_frames():
     while True:
         success, frame = camera.read()
@@ -15,16 +14,6 @@ def generate_frames():
             ret, buffer = cv2.imencode('.jpg',frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'b'Content-type: image/jpeg \r\n\r\n' + frame + b'\r\n')
-
-# def generate_frames():
-#     while True:
-#         success, frame = camera.read()
-#         if not success:
-#             break
-#         else:
-#             ret, buffer = cv2.imencode('.jpg',frame)
-#             frame = buffer.tobytes()
-#             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
 
 @app.route('/')
 def index():
