6b900805c47023d1cd12bb956d4a8a0923a240b4
Aman Shrestha <ashrest2@u.rochester.edu>
2021-04-03 23:56:47 -0400

    Tested working with video and set up for REST api

--- application/defaultCamera.py
+++ application/defaultCamera.py
@@ -4,12 +4,21 @@ import cv2
 
 
 camera = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml') #Loading the model
+
 def generate_frames():
     while True:
         success, frame = camera.read()
         if not success:
             break
         else:
+            grayScale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(grayScale, scaleFactor=1.5, minNeighbors=5)
+            for (x,y,w,h) in faces:
+                color = (255,0,0)
+                stroke = 2
+                # Rectangle that shows the face
+                cv2.rectangle(frame, (x, y), (x+w, y+h), color, stroke)
             ret, buffer = cv2.imencode('.jpg',frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'b'Content-type: image/jpeg \r\n\r\n' + frame + b'\r\n')
@@ -18,7 +27,7 @@ def generate_frames():
 
 @app.route('/video')
 def index():
-    return render_template('index.html')
+    return render_template('mainCamera.html')
 
 @app.route('/video_feed')
 def video_feed():
